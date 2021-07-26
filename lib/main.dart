2969cce780230964233ec0a5c3d98efc1242d49d
Parikshit Misra <perryizgr8@live.in>
2021-07-26 23:17:56 +0530

    All settings added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IPsec Overhead Calculator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'IPsec Overhead Calculator'),
     );
   }
 }
@@ -46,18 +48,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var greEnabled = false;
+  var tunnelKeyEnabled = false;
+  var natEnabled = false;
+  // mode 0 = transport
+  // mode 1 = tunnel
+  var ipsecMode = 0;
+  var chosenEspValue = 'AES128 + SHA1';
 
   @override
   Widget build(BuildContext context) {
@@ -67,47 +64,206 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 3,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: <Widget>[
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Unencrypted packet info',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          labelText: 'Unencrypted IP packet size in bytes',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'GRE settings',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Text('GRE over IPsec'),
+                          Switch(value: greEnabled, onChanged: onGreToggle),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Text('Tunnel keying'),
+                          SizedBox(
+                            width: 9.0,
+                          ),
+                          Switch(
+                            value: tunnelKeyEnabled,
+                            onChanged: onTunnelKeyToggle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'IPsec settings',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Text('NAT traversal'),
+                          Switch(value: natEnabled, onChanged: onNatToggle),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Text('IPsec mode:'),
+                          SizedBox(
+                            width: 9.0,
+                          ),
+                          Text('Transport'),
+                          Radio(
+                            value: 0,
+                            groupValue: ipsecMode,
+                            onChanged: onModeChanged,
+                          ),
+                          Text('Tunnel'),
+                          Radio(
+                            value: 1,
+                            groupValue: ipsecMode,
+                            onChanged: onModeChanged,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Column(
+                        children: [
+                          Text('ESP encryption and integrity'),
+                          DropdownButton<String>(
+                            value: chosenEspValue,
+                            items: <String>[
+                              'AES128 + SHA1',
+                              'AES128 + SHA256',
+                              'AES256 + SHA1',
+                              'AES256 + SHA256',
+                              'AES128 GCM64',
+                              'AES128 GCM128',
+                              'AES256 GCM128',
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (var value) {
+                              setState(() {
+                                chosenEspValue = value as String;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void onGreToggle(bool value) {
+    if (greEnabled == false) {
+      setState(() {
+        greEnabled = true;
+      });
+    } else {
+      setState(() {
+        greEnabled = false;
+      });
+    }
+  }
+
+  void onTunnelKeyToggle(bool value) {
+    if (tunnelKeyEnabled == false) {
+      setState(() {
+        tunnelKeyEnabled = true;
+      });
+    } else {
+      setState(() {
+        tunnelKeyEnabled = false;
+      });
+    }
+  }
+
+  void onNatToggle(bool value) {
+    if (natEnabled == false) {
+      setState(() {
+        natEnabled = true;
+      });
+    } else {
+      setState(() {
+        natEnabled = false;
+      });
+    }
+  }
+
+  void onModeChanged(var value) {
+    setState(() {
+      ipsecMode = value;
+    });
   }
 }
