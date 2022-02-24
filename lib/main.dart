c2391890f4da2d2cb4e4c2a4dfae821298ec6ab6
Parikshit Misra <perryizgr8@live.in>
2022-02-24 11:39:04 +0530

    Install firebase
    1. initialize Firebase
    2. flutter pub add firebase_analytics
    
    Look at https://firebase.flutter.dev/docs/analytics/overview.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
